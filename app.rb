3d70f42fb7cde33ae81e7cdef577dde7eb4f53fa
cdg77 <cdegregorio@gmail.com>
2015-08-28 15:32:03 -0700

    it will got to the index page and list any bands in the database and it will allow the user to add new bands to the database and display them on the index page.(4 passing specs)

--- app.rb
+++ app.rb
@@ -4,5 +4,15 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @bands = Band.all()
+
   erb(:index)
+end
+
+post('/new_band') do
+  name = params.fetch('name')
+  new_band = Band.new({:name => name, :id => nil})
+  new_band.save()
+
+  redirect('/')
 end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,15 @@
+require('spec_helper')
+
+describe('the index path', {:type => :feature}) do
+  it('allows the user to see a list of bands on the index') do
+    visit('/')
+    expect(page).to have_content('Band - Venue Tracker')
+  end
+
+  it('allows the user to add a band') do
+    visit('/')
+    fill_in('name', :with => 'Mumford and Sons')
+    click_button('ADD BAND')
+    expect(page).to have_content('Mumford and Sons')
+  end
+end
