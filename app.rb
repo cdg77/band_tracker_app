bd30ae87827d3a8c7798c171dec7e0db1adfbf5f
cdg77 <cdegregorio@gmail.com>
2015-08-29 19:13:32 -0700

    it will add venues to bands and edit bands and venues

--- app.rb
+++ app.rb
@@ -25,3 +25,89 @@ post('/new_venue') do
 
   redirect('/')
 end
+
+post('/bands') do
+  name = params.fetch('name')
+  @band = Band.create({:name => name})
+
+  redirect('/')
+end
+
+get('/bands/:id') do
+  @venues = Venue.all()
+  # @websites = Website.all()
+  @bands = Band.all()
+  @band = Band.find(params.fetch('id').to_i())
+  erb(:band)
+end
+
+post('/bands/:id') do
+  @bands = Band.all()
+  @venues = Venue.all()
+  @band = Band.find(params.fetch('id').to_i())
+  place = params.fetch('place')
+  @band.venues.create(:place => place)
+  # @websites = Website.all()
+  # address = params.fetch('address')
+  # @band.websites.create(:address => address)
+  redirect("/bands/#{@band.id()}")
+  # redirect('/bands/'.concat(@band.id().to_s()))
+end
+
+patch('/bands/:id') do
+  @bands = Band.all()
+  @band = Band.find(params.fetch('id').to_i())
+  name =  params.fetch('name')
+  @band.update({:name => name})
+  redirect("/bands/#{@band.id()}")
+
+end
+
+delete("/bands/:id") do
+  @bands = Band.all()
+  @band = Band.find(params.fetch('id').to_i())
+  @band.destroy()
+  redirect('/')
+end
+
+delete("/bands/:id/venues/:venue_id") do
+  @bands = Band.all()
+  @band = Band.find(params.fetch('id').to_i())
+  @venues = Venue.all()
+  @venue = Venue.find(params.fetch('venue_id').to_i())
+  @venue.destroy()
+  redirect("/bands/#{@band.id()}")
+end
+
+get("/venues/:id") do
+@venues = Venue.all()
+@venue = Venue.find(params.fetch('id').to_i())
+@bands = Band.all()
+erb(:venue)
+end
+
+post('/venues/:id') do
+  @venues = Venue.all()
+  @bands = Band.all()
+  @venue = Venue.find(params.fetch('id').to_i())
+  name = params.fetch('name')
+  @venue.bands.create(:name => name)
+  redirect("/venues/#{@venue.id()}")
+end
+
+delete("/venues/:id/bands/:band_id") do
+  @venues = Venue.all()
+  @venue = Venue.find(params.fetch('id').to_i())
+  @bands = Band.all()
+  @band = Band.find(params.fetch('band_id').to_i())
+  @band.destroy()
+  redirect("/venues/#{@venue.id()}")
+end
+
+patch('/venues/:id') do
+  @venues =Venue.all()
+  @venue =Venue.find(params.fetch('id').to_i())
+  place =  params.fetch('place')
+  @venue.update({:place => place})
+  redirect("/venues/#{@venue.id()}")
+end
