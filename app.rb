789315aec6df6b1dadf0e27cd0c2aef37c3092d6
cdg77 <cdegregorio@gmail.com>
2015-08-29 17:12:26 -0700

    it will add venues to the homepage

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
   @bands = Band.all()
+  @venues = Venue.all()
 
   erb(:index)
 end
@@ -13,6 +14,14 @@ post('/new_band') do
   name = params.fetch('name')
   new_band = Band.new({:name => name, :id => nil})
   new_band.save()
+
+  redirect('/')
+end
+
+post('/new_venue') do
+  place = params.fetch('place')
+  new_venue = Venue.new({:place => place})
+  new_venue.save()
 
   redirect('/')
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,4 +12,11 @@ describe('the index path', {:type => :feature}) do
     click_button('ADD BAND')
     expect(page).to have_content('Mumford and Sons')
   end
+
+  it('allows the user to add a venue') do
+    visit('/')
+    fill_in('place', :with => 'The Emirates Stadium')
+    click_button('ADD VENUE')
+    expect(page).to have_content('The Emirates Stadium')
+  end
 end
